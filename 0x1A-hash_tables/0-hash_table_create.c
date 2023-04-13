1363990ad6247cc2a23bf9d3babbb40168e82859
Caesar254 <augustineodu9@gmail.com>
2023-04-12 17:38:52 -0700

    harsh tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,19 +1,5 @@
 #include "hash_tables.h"
 
-
-/**
-
- * hash_table_create - creates a hash table
-
- * @size: size of the hash table
-
- *
-
- * Return: pointer to hash table
-
- */
-
-
 hash_table_t *hash_table_create(unsigned long int size)
 
 {
